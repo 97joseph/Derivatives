cdcb6a0d843e249af67207149edb22fc21e8e837
Jeptoo080 <33089347+97joseph@users.noreply.github.com>
2022-06-23 14:34:54 +0300

    R

--- ALY6050_Module 6 Project Part II_Zhang_Y.R
+++ ALY6050_Module 6 Project Part II_Zhang_Y.R
@@ -2,7 +2,7 @@ library(mFilter)
 library(quantmod)
 
 # Data preparation
-data <- read.csv("/Users/yuyizhang/FCR/NEU/CPS/Analytics_2018/ALY 6050_Introduction to Enterprise Analytics/Week 6/Assignment 6/Week 6 Project-part 2-Data.csv", header = FALSE)
+
 hon <- as.matrix(data[, 5])
 
 # Decompose the stock price
